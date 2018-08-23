2a8d6cc64636cab96481d0f5e840dfedefef047a
Leopoldo Teixeira <leopoldomt@gmail.com>
2018-08-23 07:35:21 -0300

    código desenvolvido durante a aula de 22.08.2018 - hello world

--- 2018-08-22/app/src/main/java/br/ufpe/cin/if710/primeiraaula/MainActivity.kt
+++ 2018-08-22/app/src/main/java/br/ufpe/cin/if710/primeiraaula/MainActivity.kt
@@ -2,11 +2,45 @@ package br.ufpe.cin.if710.primeiraaula
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+/*
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+*/
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        //Código Kotlin com "sotaque" Java
+        val labelTexto = findViewById(R.id.labelTexto) as TextView
+        labelTexto.setText("Mudou no código")
+        val campoTexto = findViewById<EditText>(R.id.campoTexto)
+        val botao = findViewById<Button>(R.id.botao)
+        botao.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                labelTexto.setText(campoTexto.text)
+            }
+        })
+        //TextView labelTexto = findViewById(R.id.labelTexto);
+        */
+
+        //Acessando properties diretamente
+        labelTexto.text = "Mudou no código"
+
+        //Várias formas de setar o Listener com lambda expressions
+        //botao.setOnClickListener(View.OnClickListener { v -> labelTexto.text = campoTexto.text })
+        //botao.setOnClickListener({ v -> labelTexto.text = campoTexto.text })
+        //botao.setOnClickListener(){ v -> labelTexto.text = campoTexto.text }
+        //botao.setOnClickListener(){ labelTexto.text = campoTexto.text }
+        botao.setOnClickListener { labelTexto.text = campoTexto.text }
+
     }
 }
