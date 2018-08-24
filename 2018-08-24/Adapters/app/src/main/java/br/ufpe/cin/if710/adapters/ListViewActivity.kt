73ac226b8de15470265999b1cbf0a72d8645075b
Leopoldo Teixeira <leopoldomt@gmail.com>
2018-08-24 14:08:02 -0300

    Código desenvolvido durante a aula de 24.08.2018

--- 2018-08-24/Adapters/app/src/main/java/br/ufpe/cin/if710/adapters/ListViewActivity.kt
+++ 2018-08-24/Adapters/app/src/main/java/br/ufpe/cin/if710/adapters/ListViewActivity.kt
@@ -12,5 +12,26 @@ class ListViewActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
+
+        val opcoes = hashMapOf(
+                0 to Constants.lista,
+                1 to Constants.listaLonga,
+                2 to Constants.palavrasSimilares
+        )
+
+        btn_Troca.setOnClickListener{
+            val i = Random().nextInt(opcoes.size)
+            listaElementos.adapter = ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    opcoes[i])
+
+        }
+
+        listaElementos.setOnItemClickListener{
+            parent, _, position, id ->
+            val texto = parent.adapter.getItem(position)
+            Toast.makeText(applicationContext,texto.toString(),Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- 2018-08-24/Adapters/app/src/main/java/br/ufpe/cin/if710/adapters/ViewHolder.kt
+++ 2018-08-24/Adapters/app/src/main/java/br/ufpe/cin/if710/adapters/ViewHolder.kt
@@ -3,5 +3,12 @@ package br.ufpe.cin.if710.adapters
 import android.view.View
 import android.widget.TextView
 
-internal class ViewHolder() {
+internal class ViewHolder(row: View) {
+    var nome: TextView? = null
+    var login: TextView? = null
+
+    init {
+        this.nome = row.findViewById(R.id.nome)
+        this.login = row.findViewById(R.id.login)
+    }
 }
